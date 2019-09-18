9ab5f9a9f3dfade69bc13cab251cda8c359091f1
Saloni Kalsekar <saloniskalsekar@gmail.com>
2019-09-17 21:24:01 -0600

    bubble_insertion

--- insertionSort.py
+++ insertionSort.py
@@ -1,29 +1,31 @@
-def insertSort(l):
-    i = 1
-    j= i-1
-    while i <len(l):
-        print(l)
-        while j >= 0 and l[i] < l[j]:
-                l[i], l[j] = l[j], l[i]
-                i -= 1
-                j -= 1
-
-        i += 1
-        j += 1
 
 
-l = [6,8,1,4,10]
 
-insertSort(l)
 
+
+def insertionSort(arr):
+    key = 1
+    while key < len(arr):
+        for i in range(key,0,-1):
+            if arr[i] < arr[i - 1]:
+                arr[i], arr[i - 1] = arr[i - 1], arr[i]
+        key += 1
+    return arr
+
+
+
+
+
+
+
+l = [6,1,8, 4,10]
+
+print(insertionSort(l))
 
 
 # Iterations
+#
 # [6, 8, 1, 4, 10]
-# [6, 8, 1, 4, 10]
-# [1, 6, 8, 4, 10]
-# [1, 6, 8, 4, 10]
-# [1, 6, 8, 4, 10]
-# [1, 4, 6, 8, 10]
-# [1, 4, 6, 8, 10]
-# [1, 4, 6, 8, 10]
+# [6, 1, 8, 4, 10]
+# [6, 1, 4, 8, 10]
+# [6, 1, 4, 8, 10]
