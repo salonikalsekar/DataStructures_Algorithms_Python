6a90b4b0ea71891de1f4942a3a546536abf70338
Saloni <saloniskalsekar@gmail.com>
2019-09-21 12:02:58 -0600

    hashmap_get_delete

--- hashTableAlgo.py
+++ hashTableAlgo.py
@@ -20,18 +20,50 @@ class hashTableAlgo:
         else:
             bucket.append((key, value))
 
+    def get_val(self, key):
+        hashed_key = hash(key) % self.size
+        found = False
+        bucket = self.hash_table[hashed_key]
+        for index, record in enumerate(bucket):
+            recordkey, recordvalue = record
+            if recordkey == key:
+                found = True
+                break;
+        if found:
+            return recordvalue
+        else:
+            return "Not found"
 
+    def delete(self, key):
+        hashed_key = hash(key) % self.size
+        found = False
+        bucket = self.hash_table[hashed_key]
+        for index, record in enumerate(bucket):
+            recordkey, recordvalue = record
+            if recordkey == key:
+                found = True
+                break;
+        if found:
+            bucket.pop(index)
+            return "Deleted"
+        else:
+            return "Not found"
 
-    def get_val(self):
-        pass
 
     def __str__(self):
         return "".join(str(item) for item in self.hash_table)
 
 
 hash_table = hashTableAlgo(256)
-print(hash_table)
 hash_table.set_val('saloni@test.com', 'kkk')
-hash_table.set_val('saloni@gmail.com', 'kkk')
+hash_table.set_val('s@test.com', 'aaa')
+hash_table.set_val('a@test.com', 'sss')
+hash_table.set_val('d@test.com', 'ddd')
+result = hash_table.get_val('h@test.com')
 
+print(result)
+print(hash_table)
+
+delrec = hash_table.delete('saloni@test.com')
+print(delrec)
 print(hash_table)
